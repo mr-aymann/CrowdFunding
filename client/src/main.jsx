5ef6dbd965ee792e8e1550e60dece4eb6f06076d
mr-aymann <mr.ayman.tech@gmail.com>
2023-05-10 16:29:16 +0530

    modified src folder in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App';
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot (document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
-);
+<ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+    <Router>
+        <App/>
+    </Router>
+</ThirdwebProvider>
+)
