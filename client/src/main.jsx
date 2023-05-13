422bf5f908a565b4fcfaacdd0c74e528cd594383
mr-aymann <mr.ayman.tech@gmail.com>
2023-05-13 23:44:05 +0530

    bug:fix issue #3 Fetch-Contract-Metadata

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Sepolia } from "@thirdweb-dev/chains";
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
 import { StateContextProvider } from './context';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot (document.getElementById('root'));
 root.render(
-<ThirdwebProvider activeChainId={ Sepolia }>
+<ThirdwebProvider activeChain={ Sepolia }>
     <Router>
     <StateContextProvider>
         <App />
